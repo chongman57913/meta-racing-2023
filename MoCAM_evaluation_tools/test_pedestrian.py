2a61db5ab1b1ce8f99beb65988a1a338028421e8
chongman57913 <chongman57913@users.noreply.github.com>
2023-05-26 23:32:33 +0800

    Update test_pedestrian.py

--- MoCAM_evaluation_tools/test_pedestrian.py
+++ MoCAM_evaluation_tools/test_pedestrian.py
@@ -30,45 +30,13 @@ def main():
         # 拿到这个世界所有物体的蓝图
         bp4 = world.get_blueprint_library().find('walker.pedestrian.0001')
         blueprint_library = world.get_blueprint_library()
-        # 从浩瀚如海的蓝图中找到奔驰的蓝图
-        # ego_vehicle_bp = blueprint_library.find('vehicle.audi.a2')
-        # 给我们的车加上特定的颜色
-        # ego_vehicle_bp.set_attribute('color', '52, 192, 235')
 
-        # ego_vehicle_bp_2 = blueprint_library.find('vehicle.audi.etron')
-        # ego_vehicle_bp_2.set_attribute('color', '79, 235, 52')
-        # 找到所有可以作为初始点的位置并随机选择一个
-        # transform = random.choice(world.get_map().get_spawn_points())
-        
-        # 设置固定点
-        # transform = carla.Transform(carla.Location(x=-13, y=80, z=2), carla.Rotation(yaw=90))
-        # 在这个位置生成汽车
-        # ego_vehicle_0 = world.spawn_actor(ego_vehicle_bp, transform)
-
-        # ego_vehicle_1 = world.spawn_actor(ego_vehicle_bp_2, carla.Transform(carla.Location(x=-22.2, y=134.8, z=2), carla.Rotation(yaw=0)))
-        # ego_vehicle_1 = world.spawn_actor(ego_vehicle_bp, carla.Transform(carla.Location(x=-13, y=80, z=2), carla.Rotation(yaw=90)))
-        # at staright lane
-
-        # ego_vehicle_1 = world.spawn_actor(ego_vehicle_bp, carla.Transform(carla.Location(x=-40, y=1, z=2), carla.Rotation(yaw=90)))
-        # # at staright lane
-
-        # ego_vehicle_2 = world.spawn_actor(ego_vehicle_bp, carla.Transform(carla.Location(x=-40, y=1, z=2), carla.Rotation(yaw=90)))
-        # at start point front
-
-        # set walker as spawn_actor
-        # walker_bp = random.choice(blueprint_library.filter('walker.pedestrian.*'))
         spawn_point = carla.Transform(carla.Location(x=-12.5, y=1.5, z=0.2),carla.Rotation())
-        # spawn_point = carla.Transform(carla.Location(x=152, y=62.3, z=2),carla.Rotation(yaw=-90))
+
         pedestrain = world.spawn_actor(bp4, spawn_point)
         control = carla.WalkerControl()
         revert_flag = False
-        # weather = carla.WeatherParameters(
-        # cloudiness=80.0,
-        # precipitation=30.0,
-        # sun_altitude_angle=90.0)
-        # world.set_weather(weather)
 
-        #print(world.get_weather())
         while True:
             # world.wait_for_tick()
             control = carla.WalkerControl()
@@ -85,15 +53,8 @@ def main():
                 control.direction.x = 1
             pedestrain.apply_control(control)
             time.sleep(1)
-            # Debug Tool
-            # logging.warning(pedestrain.get_location())
-            # print(,control.direction.x,revert_flag)
         
     finally:
-        # print('\ndestroying vehicles')
-        # ego_vehicle_0.destroy()
-        # ego_vehicle_1.destroy()
-        # ego_vehicle_2.destroy()
         if pedestrain:
             pedestrain.destroy()
 
